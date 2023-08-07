5103f77c09804499cd1132572606d7ed3a76a50f
etechsam <etechsnoom@gmail.com>
2023-08-07 10:39:27 +0300

    [ADD] HT01235 add adir_purchase

---
+++ adir_purchase/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 Arkeup
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Adir Purchase',
+    'description': """
+        Purchase custom feature""",
+    'version': '16',
+    'license': 'AGPL-3',
+    'author': 'Arkeup',
+    'website': 'https://www.arkeup.com',
+    'depends': [
+    ],
+    'data': [
+        # 'views/purchase_order.xml',
+    ],
+    'demo': [
+    ],
+}
